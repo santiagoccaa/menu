ada97746b0e5224bb6c5b2b643d4f71261242800
santiagoccaa <santiagoccaa@gmail.com>
2025-08-02 12:18:38 -0500

    5 feat: aparecing loading icon in button when click in save

--- src/app/admin/[slug]/page.tsx
+++ src/app/admin/[slug]/page.tsx
@@ -4,15 +4,18 @@ import { supabase } from '@/lib/client'
 import { Product } from '@/types/product'
 import { useParams } from 'next/navigation'
 import React, { FormEvent, useEffect, useState } from 'react'
+import { TbLoader2 } from 'react-icons/tb'
 
 const InsertProduct = () => {
   const category = useParams().slug as string
+  console.log("La categoria es:" ,category);
+  
+  const [isLoading, setIsLoading] = useState(false)
 
   const [productList, setProductList] = useState<Product[]>([])
   async function fetchProducts() {
     const { data, error } = await supabase.from("productos").select("*")
-    if (error) console.error(error)
-    else setProductList(data as Product[])
+    if (!error) setProductList(data as Product[])
   }
 
   useEffect(() => {
@@ -29,20 +32,30 @@ const InsertProduct = () => {
 
   async function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
+    setIsLoading(true)
 
     const { error } = await supabase.from("productos").insert([form])
 
     if (error) {
-      console.log("ERROR", error.message);
+      setIsLoading(false)
     } else {
-      console.log("SIn errores");
-      fetchProducts()
+      await fetchProducts()
+      setTimeout(() => {
+        setForm({
+          name: "",
+          ingredients: [],
+          cost: 0,
+          category: category,
+          image: ""
+        })
+        setIsLoading(false)
+      }, 500)
     }
 
   }
   return (
     <div>
-      <form onSubmit={handleFormSubmit} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-8 h-40 overflow-hidden">
+      <form onSubmit={handleFormSubmit} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-8">
         <div>
           <label className="text-white text-sm font-bold block">Name</label>
           <input
@@ -96,7 +109,15 @@ const InsertProduct = () => {
           />
         </div>
 
-        <input type="submit" value="Guardar" className="bg-blue-500 border-1 border-blue-500 w-52 rounded-md cursor-pointer hover:bg-white hover:text-blue-500 font-bold duration-300 h-12" />
+        <button type="submit" value="Guardar" className="bg-blue-500 border-1 border-blue-500 w-52 rounded-md cursor-pointer hover:bg-white hover:text-blue-500 font-bold duration-300 h-12" >
+          {
+            isLoading
+              ?
+              <TbLoader2 className="animate-spin" />
+              :
+              "Guardar"
+          }
+        </button>
       </form>
 
       <div className="w-full border-t-2 border-white mt-4 px-4">
@@ -106,7 +127,10 @@ const InsertProduct = () => {
           <div className="text-white grid grid-cols-1 md:grid-cols-3 gap-4">
             {productList.map((cat, idx) => (
               <div key={idx} className="py-2 border-b border-white text-center">
-                {cat.name}
+                <h2>{cat.name}</h2>
+                <p>{cat.ingredients}</p>
+                <h2>{cat.cost}</h2>
+                <h2>{cat.image}</h2>
               </div>
             ))}
           </div>
