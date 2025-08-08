fd5ffc7813d84d379e5205351c4f22a65efb3b11
santiagoccaa <santiagoccaa@gmail.com>
2025-08-08 15:41:36 -0500

    16 feat: function edit and delete category or product

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -4,7 +4,7 @@ import { supabase } from "./client"
 // This functions using in Modules/AdminPage
 
 export async function fetchCategories() {
-    const { data, error } = await supabase.from("categorias").select("*")
+    const { data, error } = await supabase.from("categorias").select("*").order("id", { ascending: true })
     if (!error) return data as Category[]
 }
 
@@ -13,7 +13,7 @@ export async function saveCategory(category: Category) {
 }
 
 export async function fetchProducts(category: string) {
-    const { data, error } = await supabase.from("productos").select("*").eq("category", category)
+    const { data, error } = await supabase.from("productos").select("*").eq("category", category).order("id", { ascending: true })
     if (!error) return data as Product[]
 }
 
@@ -21,7 +21,7 @@ export async function InsertProduct(product: Product) {
     await supabase.from("productos").insert([product])
 }
 
-// -DELETE
+// ----------DELETE
 
 //Product
 export async function deleteProduct(id: number) {
@@ -32,3 +32,16 @@ export async function deleteProduct(id: number) {
 export async function deleteCategory(id: number) {
     await supabase.from("categorias").delete().eq("id", id)
 }
+
+// -----------EDIT
+
+//Producto
+export async function EditProduct(id: number, product: Partial<Product>) {
+    await supabase.from("productos").update({ name: product.name, ingredients: product.ingredients, cost: product.cost, image: product.image }).eq("id", id)
+}
+
+//Category
+export async function EditCategory(id: number, newName: string) {
+    await supabase.from("categorias").update({ name: newName }).eq("id", id)
+}
+
--- src/modules/AdminPage/AdminPage.tsx
+++ src/modules/AdminPage/AdminPage.tsx
@@ -3,16 +3,20 @@
 import { Category } from "@/types/product"
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { deleteCategory, fetchCategories } from "@/lib/service";
+import { deleteCategory, EditCategory, fetchCategories } from "@/lib/service";
 import AddCategory from "./components/AddCategory";
 import { AiFillSetting } from "react-icons/ai";
-import { TbEdit } from "react-icons/tb";
+import { TbEdit, TbLoader2 } from "react-icons/tb";
 import { GoTrash } from "react-icons/go";
+import { MdOutlineSaveAlt } from "react-icons/md";
 
 const AdminPage = () => {
 
     const [categoryegories, setCategories] = useState<Category[]>([])
-    const [editCategeory, editEditCategory] = useState<number | null>(null)
+    const [settingsCategory, setSettingsCategory] = useState<number | null>(null)
+    const [editCagory, setEditCategory] = useState<number | null>(null);
+    const [editCategoryName, setEditCategoryName] = useState("")
+    const [loading, setLoading] = useState(false)
 
     const getCategories = async () => {
         const data = await fetchCategories()
@@ -24,7 +28,7 @@ const AdminPage = () => {
     }, [])
 
     const toggleEdit = (idx: number) => {
-        editEditCategory(prev => (prev === idx ? null : idx))
+        setSettingsCategory(prev => (prev === idx ? null : idx))
     }
 
     return (
@@ -35,32 +39,85 @@ const AdminPage = () => {
                 {categoryegories.length === 0 ? (
                     <p className="text-white text-sm italic mt-4">No hay categoryegorías registradas.</p>
                 ) : (
-                    <div className="text-white grid grid-cols-1 md:grid-cols-4 gap-4">
+                    <div className="text-white grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         {categoryegories.map((category, idx) => (
                             <div key={idx} className="relative">
-                                <Link href={`/admin/${category.name}`} className="flex py-2 text-2xl capitalize bg-red-400 h-24 border-b border-white justify-center items-center rounded-lg hover:text-red-400 hover:bg-white transition-all duration-300">
-                                    {category.name}
+                                {editCagory === idx
+                                    ?
+                                    <div className="p-4 flex gap-2">
+                                        <input
+                                            type="text"
+                                            value={editCategoryName}
+                                            onChange={(e) => setEditCategoryName(e.target.value)}
+                                            className="w-full bg-white text-gray-800"
+                                        />
+                                    </div>
+                                    :
+                                    <Link href={`/admin/${category.name}`} className="flex py-2 text-2xl capitalize bg-red-400 h-24 border-b border-white justify-center items-center rounded-lg hover:text-red-400 hover:bg-white transition-all duration-300">
+                                        {category.name}
+                                    </Link>
+                                }
 
-                                </Link>
                                 <div
-                                    className={`absolute overflow-hidden bottom-2 right-2 ${editCategeory === idx ? 'w-48' : 'w-12'} h-12 rounded-full bg-red-400 transition-all duration-300 z-50`}
+                                    className={`absolute overflow-hidden bottom-2 right-2 ${settingsCategory === idx ? 'w-48' : 'w-12'} h-12 rounded-full bg-red-400 transition-all duration-300 z-50`}
                                 >
                                     <div className='relative bg-blue-400 w-full h-12'>
-                                        <button
-                                            onClick={() => toggleEdit(idx)}
-                                            className='absolute top-0 right-0 rounded-full bg-blue-400 w-12 h-12 flex justify-center items-center cursor-pointer border-1'
-                                        >
-                                            <AiFillSetting size={24} className={`${editCategeory === idx ? 'rotate-90' : ''} duration-300`} />
-                                        </button>
+                                        <div className="absolute top-0 right-0 rounded-full bg-blue-400 w-12 h-12 border-1">
+                                            {editCagory === idx
+                                                ?
+                                                <button
+                                                    onClick={() => {
+                                                        if (category.id && editCategoryName.trim() !== "") {
+                                                            setLoading(true)
+                                                            EditCategory(category.id, editCategoryName.trim());
+                                                            
+                                                            setTimeout(() => {
+                                                                setEditCategory(null);
+                                                                getCategories();
+                                                                setLoading(false)
+                                                            }, 1200)
+                                                        }
+                                                    }}
+                                                    className="w-full h-full flex justify-center items-center cursor-pointer"
+                                                    disabled={loading && true}
+                                                >
+                                                    {loading
+                                                        ?
+                                                        <TbLoader2 size={25} className="animate-spin" />
+                                                        :
+                                                        <MdOutlineSaveAlt size={25} />}
+                                                </button>
+                                                :
+                                                <button
+                                                    onClick={() => toggleEdit(idx)}
+                                                    className='w-full h-full flex justify-center items-center cursor-pointer'
+                                                >
+                                                    <AiFillSetting size={24} className={`${settingsCategory === idx ? 'rotate-90' : ''} duration-300`} />
+
+                                                </button>
+                                            }
+                                        </div>
                                         <div className={`flex gap-8 px-8 h-full items-center bg-white`}>
                                             <button className='cursor-pointer text-gray-800 0 hover:scale-110 transition-all duration-300'>
-                                                <TbEdit size={20} />
+                                                <TbEdit
+                                                    onClick={() => {
+                                                        toggleEdit(idx)
+                                                        setEditCategoryName(category.name)
+                                                        setEditCategory(idx)
+                                                    }}
+                                                    size={20}
+                                                />
                                             </button>
                                             <button
                                                 className='cursor-pointer text-gray-800 hover:scale-110 transition-all duration-300'
                                                 onClick={() => {
                                                     if (category.id) {
                                                         deleteCategory(category.id)
+                                                        setEditCategory(null);
+                                                        setSettingsCategory(null)
+                                                        setTimeout(() => {
+                                                            getCategories();
+                                                        }, 500)
                                                     }
                                                 }}
                                             >
